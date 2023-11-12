f67a9c30b7b1381789e8cc7bc34b8964111a87e7
Roman Prunko <roman.prunko24@gmail.com>
2023-11-12 15:47:05 +0200

    lab3 + screenshots

--- lab3/app.py
+++ lab3/app.py
@@ -1,16 +1,36 @@
-# This is a sample Python script.
+import os
+from flask import Flask, render_template, request
+from datetime import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    os_name = os.name  # Операційна система
+    user_agent = request.user_agent.string  # User Agent
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')  # Поточний час
+    return render_template('page1.html', os_name=os_name, user_agent=user_agent, current_time=current_time)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/page1')
+def page1():
+    os_name = os.name
+    user_agent = request.user_agent.string
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('page1.html', os_name=os_name, user_agent=user_agent, current_time=current_time)
 
+@app.route('/page2')
+def page2():
+    os_name = os.name
+    user_agent = request.user_agent.string
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('page2.html', os_name=os_name, user_agent=user_agent, current_time=current_time)
 
-# Press the green button in the gutter to run the script.
+@app.route('/page3')
+def page3():
+    os_name = os.name
+    user_agent = request.user_agent.string
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template('page3.html', os_name=os_name, user_agent=user_agent, current_time=current_time)
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run()
