d95e60c3c0f0abf55ca3426ae0b5949bcc873abd
Roman Prunko <roman.prunko24@gmail.com>
2023-11-14 01:44:49 +0200

    commit 2 || step 5

--- lab4/app/views.py
+++ lab4/app/views.py
@@ -1,6 +1,7 @@
 from flask import request, render_template, redirect, url_for, make_response, session, jsonify
 from app import app
 import json
+import datetime
 
 # Завантаження користувачів з файлу JS
 with open(r"D:\labs_python\lab4\app\static\js\users.js", "r") as file:
@@ -32,11 +33,54 @@ def login():
     return render_template("login.html", error=None)
 
 
-@app.route("/info")
+@app.route("/info", methods=["GET", "POST"])
 def info():
     # Перевірка, чи користувач увійшов у систему
     if "username" in session:
-        return render_template("info.html", username=session["username"])
+        username = session["username"]
+
+        # Обробка POST-запиту для роботи з кукі
+        if request.method == "POST":
+            key = request.form.get("key")
+            value = request.form.get("value")
+            expiry_date = request.form.get("expiry_date")
+
+            # Створення кукі
+            response = make_response(render_template("info.html", username=username))
+
+            if key and value:
+                response.set_cookie(key, value, expires=expiry_date)
+                # Зберігаємо час створення та термін дії кукі у додаткових кукі
+                response.set_cookie(key + '_created', str(int(datetime.datetime.now().timestamp())))
+                response.set_cookie(key + '_expires',
+                                    str(int(datetime.datetime.strptime(expiry_date, "%Y-%m-%dT%H:%M").timestamp())))
+
+            return response
+
+        # Видалення кукі за ключем
+        delete_key = request.args.get("delete_key")
+        if delete_key:
+            response = make_response(render_template("info.html", username=username))
+            response.delete_cookie(delete_key)
+            response.delete_cookie(delete_key + '_created')
+            response.delete_cookie(delete_key + '_expires')
+            return response
+
+        # Виведення поточних кукі у таблиці
+        cookies_data = []
+        for key, value in request.cookies.items():
+            creation_time = datetime.datetime.fromtimestamp(int(request.cookies.get(key + '_created', 0)))
+            expiry_time = datetime.datetime.fromtimestamp(int(request.cookies.get(key + '_expires', 0)))
+
+            cookies_data.append({
+                'key': key,
+                'value': value,
+                'creation_time': creation_time,
+                'expiry_time': expiry_time,
+            })
+
+        return render_template("info.html", username=username, cookies_data=cookies_data)
+
     else:
         return redirect(url_for("login"))
 
