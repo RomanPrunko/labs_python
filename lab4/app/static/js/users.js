90b438df14da48dd966cec082bbb93c7088cf19d
RomanPrunko <147647543+RomanPrunko@users.noreply.github.com>
2023-11-16 19:23:04 +0200

    .

--- lab4/app/static/js/users.js
+++
@@ -1,7 +0,0 @@
-{
-  "users": [
-    {"username": "RomanPrunko", "password": "password123123"},
-    {"username": "RomanPrunkoPNU", "password": "password123123"},
-    {"username": "romaaan", "password": "password2"}
-  ]
-}
